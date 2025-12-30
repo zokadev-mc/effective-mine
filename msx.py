97d88e7fc6b969fc6366b321633447054ae53d32
zokadev-mc <140729730+zokadev-mc@users.noreply.github.com>
2025-12-30 19:56:10 +0000

    [🌙] Historial reiniciado 2025-12-30

--- msx.py
+++ msx.py
@@ -9,7 +9,7 @@
 # 1. Instala la extensión de Python
 # 2. Haz click al botón de arriba a la derecha (►)
 
-# Si no aparece el botón, reinicia la página o cambia de navegador.
+# Si no aparece el botón, reinicia la página o cambia de navegador.1
 
 
 
